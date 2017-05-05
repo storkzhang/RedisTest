aed31677fa6764dd0d4276fac204ac969f8a43e2
zhangpeng1 <zhangpeng1@xiaomi.com>
2017-05-05 11:30:03 +0800

    add code for testing redis cluster

--- src/main/java/com/miui/testredis/TestRedis.java
+++ src/main/java/com/miui/testredis/TestRedis.java
@@ -1,7 +1,58 @@
 package com.miui.testredis;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
 /**
  * Created by storkzhang on 17-5-4.
  */
 public class TestRedis {
+    private HashSet<HostAndPort> mClusterNodes;
+    private JedisPoolConfig mConfig;
+    private JedisCluster mJedisCluster;
 
+    private void genClusterNode() {
+        mClusterNodes = new HashSet<HostAndPort>();
+        mClusterNodes.add(new HostAndPort("127.0.0.1", 7001));
+        mClusterNodes.add(new HostAndPort("127.0.0.1", 7002));
+        mClusterNodes.add(new HostAndPort("127.0.0.1", 7003));
+        mClusterNodes.add(new HostAndPort("127.0.0.1", 7004));
+        mClusterNodes.add(new HostAndPort("127.0.0.1", 7005));
+        mClusterNodes.add(new HostAndPort("127.0.0.1", 7006));
+    }
+
+    private void genJedisConfig() {
+        mConfig = new JedisPoolConfig();
+        mConfig.setMaxTotal(1000);
+        mConfig.setMaxIdle(100);
+        mConfig.setTestOnBorrow(true);
+    }
+
+    public void clusterInit() {
+        genClusterNode();
+        genJedisConfig();
+        mJedisCluster = new JedisCluster(mClusterNodes, 5000, mConfig);
+    }
+
+    private void setKey(String key, String value) {
+        mJedisCluster.set(key, value);
+    }
+
+    private String getKey(String key) {
+        return mJedisCluster.get(key);
+    }
+
+
+    public static void main(String args[]) {
+        String value;
+        TestRedis testRedis = new TestRedis();
+        testRedis.genJedisConfig();
+        testRedis.genClusterNode();
+        testRedis.clusterInit();
+        testRedis.setKey("mykey", "myvalue");
+        value = testRedis.getKey("mykey");
+        System.out.println(value);
+
+    }
 }
